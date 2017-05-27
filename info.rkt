29974d9cc9ed8fe13be15079c102d80180407cd0
William J. Bowman <wjb@williamjbowman.com>
2017-05-27 19:37:35 -0400

    Updated version

--- info.rkt
+++ info.rkt
@@ -3,5 +3,5 @@
 (define deps '("base" ("redex-lib" #:version "1.11") "redex-chk"))
 (define build-deps '())
 (define pkg-desc "model of CIC.")
-(define version "1.0")
+(define version "2.0")
 (define pkg-authors '(wilbowma))
